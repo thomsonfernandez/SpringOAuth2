2fa2314c443723aa8c959bce85739e0f8842b54a
thomsonfernandez <thomsonfernandez@users.noreply.github.com>
2020-02-22 12:28:38 +0400

    changes for auth2

--- src/main/java/com/oauth2/googleapi/AppSecurityConfig.java
+++ src/main/java/com/oauth2/googleapi/AppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.oauth2.googleapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Sso
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter
 {
 	
@@ -22,15 +24,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-	@Bean
-	public AuthenticationProvider authProvider() 
-	{
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
-//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-		provider.setPasswordEncoder(new BCryptPasswordEncoder());
-		return provider;
-	}
+	/*
+	 * @Bean public AuthenticationProvider authProvider() {
+	 * DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+	 * provider.setUserDetailsService(userDetailsService); //
+	 * provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+	 * provider.setPasswordEncoder(new BCryptPasswordEncoder()); return provider; }
+	 */
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
